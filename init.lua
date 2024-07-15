a41529bf25ea70d719594d238953f3d81d0b07eb
Eric Correia <correia.eh@gmail.com>
2024-07-15 19:56:53 -0400

    kanagawa dragon

--- init.lua
+++ init.lua
@@ -349,9 +349,9 @@ autocmd('LspAttach', {
 
         -- None of this semantics tokens business.
         -- https://www.reddit.com/r/neovim/comments/143efmd/is_it_possible_to_disable_treesitter_completely/
-        if client and client.server_capabilities.semanticTokensProvider then
-            client.server_capabilities.semanticTokensProvider = nil
-        end
+        -- if client and client.server_capabilities.semanticTokensProvider then
+        --     client.server_capabilities.semanticTokensProvider = nil
+        -- end
     end,
 })
 
@@ -697,8 +697,8 @@ require('lazy').setup {
         version = '*', -- Use the latest tagged version
         opts = {},     -- This causes the plugin setup function to be called
         keys = {
-            { '<C-Down>',      '<Cmd>MultipleCursorsAddDown<CR>',          mode = { 'n', 'i' } },
-            { '<A-c>',         '<Cmd>MultipleCursorsAddDown<CR>' },
+            { '<C-c>',         '<Cmd>MultipleCursorsAddDown<CR>',          mode = { 'n', 'i' } },
+            { '<C-c>',         '<Cmd>MultipleCursorsAddDown<CR>' },
             -- {"<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = {"n", "i"}},
             -- {"<C-k>", "<Cmd>MultipleCursorsAddUp<CR>"},
             { '<C-LeftMouse>', '<Cmd>MultipleCursorsMouseAddDelete<CR>',   mode = { 'n', 'i' } },
@@ -1038,8 +1038,8 @@ require('lazy').setup {
                 -- Autoinstall languages that are not installed
                 auto_install = true,
                 -- with gruvbox theme set this to false
-                highlight = { enable = false },
-                -- highlight = { enable = true },
+                -- highlight = { enable = false },
+                highlight = { enable = true },
                 -- indent = { enable = true },
                 incremental_selection = {
                     enable = true,
@@ -1070,27 +1070,62 @@ require('lazy').setup {
         end,
     },
     {
+        "rebelot/kanagawa.nvim",
+        lazy = false,    -- load at start
+        priority = 1000, -- load first
+        config = function()
+            -- Default options:
+            require('kanagawa').setup({
+                compile = false, -- enable compiling the colorscheme
+                undercurl = true, -- enable undercurls
+                commentStyle = { italic = true },
+                functionStyle = {},
+                keywordStyle = { italic = true },
+                statementStyle = { bold = true },
+                typeStyle = {},
+                transparent = false, -- do not set background color
+                dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+                terminalColors = true, -- define vim.g.terminal_color_{0,17}
+                colors = { -- add/modify theme and palette colors
+                    palette = {},
+                    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+                },
+                overrides = function(colors) -- add/modify highlights
+                    return {}
+                end,
+                theme = "dragon", -- Load "wave" theme when 'background' option is not set
+                background = { -- map the value of 'background' option to a theme
+                    dark = "dragon", -- try "dragon" !
+                    light = "lotus"
+                },
+            })
+
+            -- setup must be called before loading
+            vim.cmd("colorscheme kanagawa")
+        end
+    },
+    {
         "wincent/base16-nvim",
         lazy = false,    -- load at start
         priority = 1000, -- load first
         config = function()
-            vim.cmd([[colorscheme base16-gruvbox-dark-hard]])
-            vim.o.background = 'dark'
+            -- vim.cmd([[colorscheme base16-gruvbox-dark-hard]])
+            -- vim.o.background = 'dark'
             -- XXX: hi Normal ctermbg=NONE
             -- Make comments more prominent -- they are important.
             -- local bools = vim.api.nvim_get_hl(0, { name = 'Boolean' })
             -- vim.api.nvim_set_hl(0, 'Comment', bools)
             -- Make it clearly visible which argument we're at.
-            local marked = vim.api.nvim_get_hl(0, { name = 'PMenu' })
-            vim.api.nvim_set_hl(0, 'LspSignatureActiveParameter',
-                { fg = marked.fg, bg = marked.bg, ctermfg = marked.ctermfg, ctermbg = marked.ctermbg, bold = true })
+            -- local marked = vim.api.nvim_get_hl(0, { name = 'PMenu' })
+            -- vim.api.nvim_set_hl(0, 'LspSignatureActiveParameter',
+            --     { fg = marked.fg, bg = marked.bg, ctermfg = marked.ctermfg, ctermbg = marked.ctermbg, bold = true })
             -- XXX
             -- Would be nice to customize the highlighting of warnings and the like to make
             -- them less glaring. But alas
             -- https://github.com/nvim-lua/lsp_extensions.nvim/issues/21
             -- call Base16hi("CocHintSign", g:base16_gui03, "", g:base16_cterm03, "", "", "")
-            local visual = vim.api.nvim_get_hl(0, { name = "Visual" })
-            vim.api.nvim_set_hl(0, '@variable', { fg = visual.fg, })
+            -- local visual = vim.api.nvim_get_hl(0, { name = "Visual" })
+            -- vim.api.nvim_set_hl(0, '@variable', { fg = visual.fg, })
         end
     },
     -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
