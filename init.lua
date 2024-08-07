756e5c60a382350a611eee0c1a7e2df0ef3dceee
eh <correia.eh@gmail.com>
2024-08-07 17:41:35 -0400

    Rasmus is the best!

--- init.lua
+++ init.lua
@@ -966,35 +966,9 @@ require('lazy').setup {
         end
     },
     {
-        "rebelot/kanagawa.nvim",
+        "kvrohit/rasmus.nvim",
         config = function()
-            require('kanagawa').setup({
-                compile = false, -- enable compiling the colorscheme
-                undercurl = true, -- enable undercurls
-                commentStyle = { italic = false },
-                functionStyle = {},
-                keywordStyle = { italic = false },
-                statementStyle = { bold = true },
-                typeStyle = {},
-                transparent = false, -- do not set background color
-                dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-                terminalColors = true, -- define vim.g.terminal_color_{0,17}
-                colors = { -- add/modify theme and palette colors
-                    palette = {},
-                    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-                },
-                overrides = function(colors) -- add/modify highlights
-                    return {}
-                end,
-                theme = "dragon", -- Load "wave" theme when 'background' option is not set
-                background = { -- map the value of 'background' option to a theme
-                    dark = "dragon", -- try "dragon" !
-                    light = "lotus"
-                },
-            })
-
-            -- setup must be called before loading
-            vim.cmd("colorscheme kanagawa")
+            vim.cmd([[colorscheme rasmus]])
         end
     },
     {
@@ -1011,39 +985,6 @@ require('lazy').setup {
             })
             -- vim.cmd([[colorscheme rose-pine]])
         end
-    },
-    {
-        "ellisonleao/gruvbox.nvim",
-        name = "gruvbox",
-        config = function()
-            require("gruvbox").setup({
-                terminal_colors = true, -- add neovim terminal colors
-                undercurl = true,
-                underline = false,
-                bold = true,
-                italic = {
-                    strings = false,
-                    emphasis = false,
-                    comments = false,
-                    operators = false,
-                    folds = false,
-                },
-                strikethrough = true,
-                invert_selection = false,
-                invert_signs = false,
-                invert_tabline = false,
-                invert_intend_guides = false,
-                inverse = true,    -- invert background for search, diffs, statuslines and errors
-                contrast = "hard", -- can be "hard", "soft" or empty string
-                palette_overrides = {},
-                overrides = {},
-                dim_inactive = false,
-                transparent_mode = true,
-            })
-
-            -- vim.o.background = "dark"
-            -- vim.cmd([[colorscheme gruvbox]])
-        end,
     },
     {
         'projekt0n/github-nvim-theme',
