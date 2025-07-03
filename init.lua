d7e8bf24c76cc02a772a7820976e470dc6b609b3
EricHenry <correia.eh@gmail.com>
2025-07-03 17:36:30 -0400

    more cleanup

--- init.lua
+++ init.lua
@@ -275,31 +275,6 @@ vim.keymap.set('n', '<leader>co',
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 -------------------------------------------------------------------------------
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
--- vim.api.nvim_create_autocmd('TextYankPost', {
--- 	desc = 'Highlight when yanking (copying) text',
--- 	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
--- 	callback = function()
--- 		vim.highlight.on_yank()
--- 	end,
--- })
---
--- vim.api.nvim_create_autocmd('BufReadPost', {
--- 	pattern = '*',
--- 	callback = function(ev)
--- 		if vim.fn.line '\'"' > 1 and vim.fn.line '\'"' <= vim.fn.line '$' then
--- 			-- except for in git commit messages
--- 			-- https://stackoverflow.com/questions/31449496/vim-ignore-specifc-file-in-autocommand
--- 			if not vim.fn.expand('%:p'):find('.git', 1, true) then
--- 				vim.cmd 'exe "normal! g\'\\""'
--- 			end
--- 		end
--- 	end,
--- })
-
-
 local augroup = vim.api.nvim_create_augroup
 local ehGroup = augroup("eh", {})
 
